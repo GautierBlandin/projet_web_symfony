b84c2a40d90d215a34e871364cd19480224ca9ea
Olivia Walsh <walsholivia7@gmail.com>
2020-10-15 10:11:13 +0200

    form changes

--- src/Controller/AdsController.php
+++ src/Controller/AdsController.php
@@ -43,19 +43,51 @@ class AdsController extends AbstractController
      */
     public function showAds()
     {
+        $em = $this->getDoctrine()->getManager();  
         $ads = $this->getDoctrine()->getRepository(ads::class)->findAll();
+ 
+        $formsView = array();
+        $forms = array();
+ 
+        foreach ($ads as &$ad) {
+            $apply = new Apply();
 
+            $form = $this->createForm(ApplyType::class, $apply);
+
+            array_push($forms, $form);
+            array_push($formsView, $form->createView());
+ 
+            $form->handleRequest($request);
+ 
+            if($form->isSubmitted() && $form->isValid()) {
+                $apply = $form->getData();
+                $apply->setCreatedAt(new \DateTime('now'));
+                
+                
+                $em->persist($apply);
+                $em->flush();
+                return $this->redirectToRoute('ads_show');
+            }
+            
+        }
+ 
+        $forms = array_values($forms);
+        
+ 
         if (!$ads) {
             throw $this->createNotFoundException(
                 'No ads found');
         }
         else {
             return $this->render('test.html.twig', [
+                
                 // this array defines the variables passed to the template,
                 // where the key is the variable name and the value is the variable value
                 // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
-
+ 
             'ads' => $ads,
+            'forms' => $formsView
+ 
             ]);
         }    
     }
